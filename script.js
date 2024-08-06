fda0a8d642b2b5844f635438e51ead538cc459a9
waqasaya-z <waqasayaz@icloud.com>
2024-08-06 23:40:37 +0500

    designed bird

---
+++ script.js
@@ -0,0 +1,25 @@
+import { updateBird, setupBird } from "./bird.js";
+
+document.addEventListener("keypress", handleStart, { once: true });
+const title = document.querySelector("[data-title]");
+
+let lastTime;
+function updateLoop(time) {
+  if (lastTime == null) {
+    lastTime = time;
+    window.requestAnimationFrame(updateLoop);
+    return;
+  }
+  const delta = time - lastTime;
+  updateBird(delta);
+  lastTime = time;
+  window.requestAnimationFrame(updateLoop);
+}
+
+function handleStart() {
+  title.classList.add("hide");
+  setupBird()
+  window.requestAnimationFrame(updateLoop);
+}
+
+function handleLost() {}
